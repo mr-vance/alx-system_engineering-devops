f0b91efaba736985c2bc0fd00db444a0705188ab
Vance <muchongo.vance@gmail.com>
2024-07-20 21:47:49 +0000

    remake 0

--- 0x06-regular_expressions/0-simply_match_holberton.rb
+++ 0x06-regular_expressions/0-simply_match_holberton.rb
@@ -1,2 +1,16 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/Holberton/).join
+
+# Check if an argument is provided
+if ARGV.empty?
+  puts ""
+  exit
+end
+
+# Input argument
+input = ARGV[0]
+
+# Use the regular expression to find matches
+matches = input.scan(/School/)
+
+# Print the matches concatenated together
+puts matches.join
