81541c9a6bbabbc96039b0bc49ea8a7412aea8c2
Vance <muchongo.vance@gmail.com>
2024-07-20 21:55:41 +0000

    remake of 0000

--- 0x06-regular_expressions/0-simply_match_holberton.rb
+++ 0x06-regular_expressions/0-simply_match_holberton.rb
@@ -1,8 +1,2 @@
 #!/usr/bin/env ruby
-
-# Ensure there's input to process
-if ARGV[0]
-    # Match all instances of "School" and concatenate
-    puts ARGV[0].scan(/School/).join
-  end
-  
+puts ARGV[0].scan(/School/).join
