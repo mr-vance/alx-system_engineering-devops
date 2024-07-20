60a05912fc6ed5fe4b5cbcb6de7406c58bb593b5
Vance <muchongo.vance@gmail.com>
2024-07-20 21:51:59 +0000

    remake of 00

--- 0x06-regular_expressions/0-simply_match_holberton.rb
+++ 0x06-regular_expressions/0-simply_match_holberton.rb
@@ -1,16 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if an argument is provided
-if ARGV.empty?
-  puts ""
-  exit
-end
-
-# Input argument
-input = ARGV[0]
-
-# Use the regular expression to find matches
-matches = input.scan(/School/)
-
-# Print the matches concatenated together
-puts matches.join
+puts ARGV[0].scan(/Holberton/).join
---
+++ 0x06-regular_expressions/README.md
@@ -0,0 +1,10 @@
+# Regex Project
+
+This project contains a Ruby script that matches the word "School" in a given input string using a regular expression. The script uses the Oniguruma regex library to perform matching.
+
+## Usage
+
+To execute the script, run:
+
+```bash
+./0-simply_match_school.rb "Your input string here"
